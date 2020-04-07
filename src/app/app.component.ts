62fc3a7cb171c878cddcd3b3c7f527e79eedd343
UlmoLordofWater <wesley.duris@gmail.com>
2020-04-07 03:43:46 -0400

    added list & some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MeanCourse';
+  storedPosts = [];
+
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
