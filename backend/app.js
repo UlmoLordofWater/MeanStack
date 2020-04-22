8a358a41c515ffc97e1bee84930be173a7dda605
UlmoLordofWater <wesley.duris@gmail.com>
2020-04-21 21:50:59 -0400

    added ui elements

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, X-REquested-With, Content-Type, Accept'
+      'Origin, X-REquested-With, Content-Type, Accept, Authorization'
     );
   res.setHeader('Access-Control-Allow-Methods',
       'GET, POST, PATCH, PUT, DELETE, OPTIONS'
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,6 +23,10 @@ export class HeaderComponent implements OnInit{
     }));
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
