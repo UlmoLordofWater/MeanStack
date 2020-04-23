ee21140f3d5b63969116409d0ede9f09dc3288f4
UlmoLordofWater <wesley.duris@gmail.com>
2020-04-23 14:47:04 -0400

    added localstorage of token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -43,9 +43,7 @@ export class AuthService {
         this.token = token
         if (token) {
           const expiresInDuration = response.expiresIn
-          this.tokenTimer = setTimeout(() => {
-            this.logout();
-          }, expiresInDuration * 1000);
+          this.setAuthTimer(expiresInDuration);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           const now = new Date();
@@ -54,6 +52,21 @@ export class AuthService {
           this.router.navigate(['/']);
         }
       });
+  }
+
+  autoAuthUser() {
+    const authInformation = this.getAuthData();
+    if (!authInformation) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+    if(expiresIn > 0) {
+      this.token = authInformation.token;
+      this.isAuthenticated = true;
+      this.setAuthTimer(expiresIn / 1000);
+      this.authStatusListener.next(true);
+    }
   }
 
   logout() {
@@ -65,6 +78,13 @@ export class AuthService {
     this.router.navigate(['/']);
   }
 
+  private setAuthTimer(duration: number) {
+    console.log("Setting timer " + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
   private saveAuthData(token: string, expirationDate: Date) {
     localStorage.setItem('token', token);
     localStorage.setItem('expiration' , expirationDate.toISOString())
@@ -72,5 +92,17 @@ export class AuthService {
 
   private cleatAuthData() {
     localStorage.removeItem('token');
+  }
+
+  private getAuthData(){
+    const token = localStorage.getItem("token");
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    }
   }
 }
